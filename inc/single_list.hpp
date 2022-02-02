1fa8fb3cd5d1df9c5e9642e79b71af0df44da72f
ofir <98081542+ofirmor1@users.noreply.github.com>
2022-02-02 08:29:27 +0200

    BigInteger hpp

--- inc/single_list.hpp
+++ inc/single_list.hpp
@@ -4,6 +4,8 @@
 class Node
 {
 public:
+    Node();
+    Node(int a_data, Node* a_next);
     int getData();
     Node* getNext();
     void setData(int a_val);
@@ -19,11 +21,14 @@ class ListItr
 public:
     ListItr();
     ListItr(Node* a_node);
+    bool operator!=(ListItr that) const;
 
     int get();
+    void set(int a_val);
     ListItr next();
-    bool equals(const ListItr &a_first) const;
-
+    bool equal(const ListItr &a_other) const;
+    bool notEqual(const ListItr &a_other) const;
+    bool search(Node* a_node, int a_key) const;
 
 private:
     Node* m_currNode;
@@ -39,11 +44,13 @@ public:
     LinkedList& operator=(LinkedList const& a_list);
 
     void add(int a_val);
-    void remove();
+    int remove();
+    Node* getHead();
     ListItr begin() const;
     ListItr end() const;
     size_t first() const;
     size_t last() const;
+    LinkedList intersection(LinkedList& a, LinkedList& b);
 
 
     int size() const;
@@ -51,14 +58,51 @@ public:
     void printList() const;
     void initListMemb();
 
-    static void copyList();
+private:
+    Node* cutHead();
+    int unbox(Node* const a_node);
 
 private:
     Node* m_head;
     Node* m_tail;
     int m_size;
 };
-// #include "../inc/inl/single_list.hxx"
+
+class BigInteger
+{
+public:
+    BigInteger();
+
+    ~BigInteger();
+    BigInteger(const BigInteger& a_big);
+    BigInteger& operator=(const BigInteger&);
+
+    BigInteger(int a_num);
+    BigInteger(long a_num);
+    BigInteger(char a_num);
+
+private:
+    BigInteger add(BigInteger& a_num);
+    BigInteger sub(BigInteger& a_num);
+    BigInteger mul(BigInteger& a_num);
+
+    void toList(int a_num);
+    void toList(long a_num);
+    void toList(char a_num);
+
+    bool isPositive() const;
+    int getSize() const;
+
+    char* toString();
+    bool isEmpty();
+
+
+private:
+    LinkedList m_digits;
+    bool m_positive;
+    int m_size;
+};
+
 #include "single_list.hxx"   
 
 #endif /*ifndef SINGLE_LIST*/
